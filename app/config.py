9b73dfe8a29f247ee5ebc8b96eb2935ff9d08403
Fiv_ <nzaikar184@gmail.com>
2026-02-10 12:12:49 +0100

    config file refactioring

--- app/config.py
+++ app/config.py
@@ -1,28 +1,20 @@
 from functools import cached_property
 
-from pydantic import HttpUrl, PostgresDsn, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
     # app
     debug: bool = False
-    title: str = "backend-pet"
-    description: str = "Something useful"
+    title: str = "CarShopBackend microservice"
+    description: str = "Microservice for CarShopBackend for storing and editing photos "
     environment: str | None = "DEV"
 
     # db
     postgres_dsn: str = "postgresql+asyncpg://admin:admin123@localhost:5432/carshopdb"
     postgres_echo: bool = False
     postgres_schema: str = "photoservice"
-
-    # sentry
-    sentry_dsn: HttpUrl | None = None
-
-    # client
-    e1_endpoint: str = "http://e1-api.ua"
-    e1_token: str = "backend_pet_test"
-
+    
     # pydantic
     model_config = SettingsConfigDict(frozen=True, extra="allow", env_file=".env")
 
